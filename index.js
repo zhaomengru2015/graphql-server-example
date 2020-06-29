415cb45bd4d7c13ced00241eb9811fd85bbdffeb
mengru zhao <mengru.zhao@rea-group.com>
2020-06-29 08:59:38 +0800

    Init

---
+++ index.js
@@ -0,0 +1,31 @@
+const {gql,ApolloServer} = require("apollo-server")
+const typeDefs =`
+  type Book{
+    title: String
+    author: String
+  }
+  type Query{
+    books: [Book]
+  }
+`
+const resolvers={
+  Query: {
+    books: ()=>books
+  }
+}
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+const server = new ApolloServer({typeDefs,resolvers});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
