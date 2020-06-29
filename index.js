b4ad809c84fe6eb72d81ebe0a8d2087aec03a152
mengru zhao <mengru.zhao@rea-group.com>
2020-06-29 09:03:30 +0800

    Get book by id

--- index.js
+++ index.js
@@ -3,24 +3,29 @@ const typeDefs =`
   type Book{
     title: String
     author: String
+    id: ID
   }
   type Query{
     books: [Book]
+    book(id: ID!): Book
   }
 `
 const resolvers={
   Query: {
-    books: ()=>books
+    books: ()=>books,
+    book: (_, { id }) => books.find((book) => book.id == id),
   }
 }
 const books = [
   {
     title: 'Harry Potter and the Chamber of Secrets',
     author: 'J.K. Rowling',
+    id: 1
   },
   {
     title: 'Jurassic Park',
     author: 'Michael Crichton',
+    id: 2
   },
 ];
 
